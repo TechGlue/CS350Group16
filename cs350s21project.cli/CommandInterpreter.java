9cd4a5cf3e02072dd47988dca74b6ad0e4ded999
TechGlue <garcialuis24@hotmail.com>
2021-05-23 16:31:32 -0700

    Building out and cleaning methods for readability.

--- cs350s21project.cli/CommandInterpreter.java
+++ cs350s21project.cli/CommandInterpreter.java
@@ -1,8 +1,10 @@
 import cs350s21project.controller.CommandManagers;
 import cs350s21project.controller.command.A_Command;
 import cs350s21project.controller.command.view.CommandViewCreateWindowFront;
+import cs350s21project.controller.command.view.CommandViewCreateWindowSide;
 import cs350s21project.controller.command.view.CommandViewCreateWindowTop;
 import cs350s21project.datatype.AgentID;
+import cs350s21project.datatype.Altitude;
 import cs350s21project.datatype.Latitude;
 import cs350s21project.datatype.Longitude;
 
@@ -11,12 +13,7 @@ import java.util.Scanner;
 public class CommandInterpreter {
     public static void main(String[] args) {
         CommandInterpreter ref = new CommandInterpreter();
- /*       Scanner rf = new Scanner(System.in);
-        String f = rf.nextLine();
-        ref.evaluate(f);*/
-
-        ref.buildLatitudeCoordinate(4, 4, 1.00);
-
+        ref.buildAltitudeCoordinate(4.00);
         ref.buildView();
     }
 
@@ -50,7 +47,7 @@ public class CommandInterpreter {
     //A manager responsible for creating
     public void buildView() {
         String name = "Top Window view";
-        int size = 1000;
+        int size = 1080 *1920;
         AgentID idWindow = new AgentID("TPview");
         CommandManagers manager = new CommandManagers();
         Latitude latitudeOrigin = new Latitude(2, 4, 1.00);
@@ -66,6 +63,57 @@ public class CommandInterpreter {
         ref.execute();
     }
 
+    public void buildTopView(AgentID idWindow, CommandManagers windowManager, Latitude latitudeOrigin, Latitude latitudeExtent, Latitude latitudeInterval,
+                             Longitude longitudeOrigin, Longitude longitudeExtent, Longitude longitudeInterval)
+    {
+        //window sizing and name setup
+        String windowName = "Top View";
+        int size = 1920 * 1080;
+
+        //building
+        CommandViewCreateWindowTop TopWindow = new CommandViewCreateWindowTop(windowManager, windowName, idWindow, size, latitudeOrigin, latitudeExtent,
+                latitudeInterval, longitudeOrigin, longitudeExtent, longitudeInterval);
+
+        //execute Window
+        TopWindow.execute();
+    }
+
+    public void buildFrontView(AgentID idWindow, CommandManagers windowManager, Longitude longitudeOrigin, Longitude longitudeExtent, Longitude longitudeInterval,
+                               Altitude altitudeOrigin, Altitude altitudeExtent, Altitude altitudeAGLInterval, Altitude altitudeBGLInterval)
+    {
+        //window sizing and name setup
+        String windowName = "Front View";
+        int size = 1920 * 1080;
+
+        //building
+        CommandViewCreateWindowFront frontWindow = new CommandViewCreateWindowFront(windowManager, windowName, idWindow, size,
+                longitudeOrigin, longitudeExtent, longitudeInterval, altitudeOrigin, altitudeExtent, altitudeAGLInterval, altitudeBGLInterval);
+
+        //execute Window
+        FrontWindow.execute();
+
+    }
+
+    public void buildSideView(AgentID idWindow, CommandManagers windowManager, Latitude latitudeOrigin, Latitude latitudeExtent, Latitude latitudeInterval,
+                              Altitude altitudeOrigin, Altitude altitudeExtent, Altitude altitudeAGLInterval, Altitude altitudeBGLInterval)
+    {
+        //window sizing in pixels and name setup
+        String windowName = "Side View";
+        int size = 1920 * 1080;
+        //building
+        CommandViewCreateWindowSide sideWindow = new CommandViewCreateWindowSide(windowManager, windowName, idWindow, size, latitudeOrigin, latitudeExtent,
+                latitudeInterval, altitudeOrigin, altitudeExtent, altitudeAGLInterval, altitudeBGLInterval);
+
+        //execute Window
+        sideWindow.execute();
+    }
+
+    //temporary methods utilized for displaying what makes a latitude, longitude, and altitude object.
+    public Altitude buildAltitudeCoordinate(double initialAltitude) {
+        Altitude newAltitudeCoordinate = new Altitude(5.00);
+        System.out.println("The following coordinate has been set for Altitude: " + newAltitudeCoordinate.toString());
+        return newAltitudeCoordinate;
+    }
 
     //temporary method utilized for displaying what makes a latitude object.
     public Latitude buildLatitudeCoordinate(int degrees, int minutes, double seconds) {
